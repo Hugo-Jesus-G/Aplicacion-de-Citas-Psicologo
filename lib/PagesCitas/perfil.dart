dad49664524e041439abb797e398ea19860517a3
Hugo-Jesus-G <hugogcc19@gmail.com>
2024-12-01 17:59:08 -0600

    se agrego el botn de cerrar sesion

--- lib/PagesCitas/perfil.dart
+++ lib/PagesCitas/perfil.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:proyecto/Modelos/alumno.dart';
 import 'package:proyecto/creditos.dart';
 import 'package:proyecto/firebase/consultas.dart';
+import 'package:proyecto/login.dart';
 
 class Perfil extends StatefulWidget {
   @override
@@ -24,74 +26,135 @@ class _PerfilState extends State<Perfil> {
     });
   }
 
+  Future<void> _logout() async {
+    await FirebaseAuth.instance.signOut();
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => LoginScreen(),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Color.fromARGB(255, 255, 255, 255),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: GestureDetector(
+              onTap: _logout,
+              child: Container(
+                padding: EdgeInsets.symmetric(vertical: 8, horizontal: 12),
+                decoration: BoxDecoration(
+                  color: const Color.fromARGB(255, 247, 1, 1),
+                  borderRadius: BorderRadius.circular(20),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black45,
+                      blurRadius: 5,
+                      offset: Offset(2, 2),
+                    ),
+                  ],
+                ),
+                child: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Text(
+                      'Cerrar sesión',
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
+                    ),
+                    SizedBox(width: 8),
+                    Icon(
+                      Icons.exit_to_app,
+                      color: Colors.white,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+      body: Center(
         child: alumno != null
             ? LayoutBuilder(
                 builder: (context, constraints) {
                   return SingleChildScrollView(
-                    child: Padding(
-                      padding: EdgeInsets.symmetric(
-                          horizontal: constraints.maxWidth * 0.05),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: <Widget>[
-                          Image.asset(
+                    padding: EdgeInsets.symmetric(horizontal: 16),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: <Widget>[
+                        SizedBox(height: 40),
+                        Center(
+                          child: Image.asset(
                             "assets/images/person.png",
-                            width: constraints.maxWidth * 0.4,
-                            height: constraints.maxWidth * 0.4,
+                            width: constraints.maxWidth *
+                                0.3, // Ajustar tamaño según pantalla
+                            height: constraints.maxWidth *
+                                0.3, // Ajustar tamaño según pantalla
                           ),
-                          SizedBox(height: 20),
-                          Text(
-                            'Nombre: ${alumno!.nombre}',
-                            style: TextStyle(
-                                fontSize: constraints.maxWidth * 0.05,
-                                fontWeight: FontWeight.bold),
+                        ),
+                        SizedBox(height: 20),
+                        Text(
+                          'Nombre: ${alumno!.nombre}',
+                          style: TextStyle(
+                            fontSize: constraints.maxWidth * 0.05,
+                            fontWeight: FontWeight.bold,
                           ),
-                          Text(
-                            'Correo: ${alumno!.correo}',
-                            style: TextStyle(
-                                fontSize: constraints.maxWidth * 0.05,
-                                fontWeight: FontWeight.bold),
+                        ),
+                        Text(
+                          'Correo: ${alumno!.correo}',
+                          style: TextStyle(
+                            fontSize: constraints.maxWidth * 0.05,
+                            fontWeight: FontWeight.bold,
                           ),
-                          Text(
-                            'Teléfono: ${alumno!.telefono}',
-                            style: TextStyle(
-                                fontSize: constraints.maxWidth * 0.05,
-                                fontWeight: FontWeight.w900),
+                        ),
+                        Text(
+                          'Teléfono: ${alumno!.telefono}',
+                          style: TextStyle(
+                            fontSize: constraints.maxWidth * 0.05,
+                            fontWeight: FontWeight.w900,
                           ),
-                          Text(
-                            'Matrícula: ${alumno!.matricula}',
-                            style: TextStyle(
-                                fontSize: constraints.maxWidth * 0.05,
-                                fontWeight: FontWeight.bold),
+                        ),
+                        Text(
+                          'Matrícula: ${alumno!.matricula}',
+                          style: TextStyle(
+                            fontSize: constraints.maxWidth * 0.05,
+                            fontWeight: FontWeight.bold,
                           ),
-                          SizedBox(height: 50),
-                          ElevatedButton(
-                            style: ElevatedButton.styleFrom(
-                              backgroundColor: Color(0xFFC5E0F8),
-                              side: BorderSide(color: Colors.black),
-                              elevation: 2,
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.zero,
+                        ),
+                        SizedBox(height: 30),
+                        ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Color(0xFFC5E0F8),
+                            side: BorderSide(color: Colors.black),
+                            elevation: 2,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.zero,
+                            ),
+                          ),
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => Creditos(),
                               ),
-                            ),
-                            onPressed: () {
-                              Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                    builder: (context) => Creditos()),
-                              );
-                            },
-                            child: Text(
-                              'Acerca de la aplicación',
-                              style: TextStyle(color: Colors.black),
-                            ),
+                            );
+                          },
+                          child: Text(
+                            'Acerca de la aplicación',
+                            style: TextStyle(color: Colors.black),
                           ),
-                        ],
-                      ),
+                        ),
+                      ],
                     ),
                   );
                 },
